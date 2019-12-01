84185bc410e4a3f6a920f34762137a56fcd44153
unknown <helpimtrappedinanemailaddress@gmail.com>
2019-11-30 21:54:16 -0600

    Check for & retry failed sends.

--- boron/app.cpp
+++ boron/app.cpp
@@ -4,10 +4,14 @@
 #include <Wire.h>
 
 // Function declaration.
-void sendSms();
+bool sendSms();
 
 // Constants.
 const int BUTTON_PIN = D5;
+
+// Variables.
+// TODO: add in logic for capping number of retries.
+bool retrying = false;
 
 // Auto-connect to the Particle cloud before running any code in this file.
 // If we can't connect we can't send messages, so it doesn't matter if we end up
@@ -26,11 +30,21 @@ void setup() {
   // and never sent an SMS. Send one now just in case.
   if (digitalRead(BUTTON_PIN) == LOW) {
     Serial.println("MailBoy woke up with button already depressed.");
-    sendSms();
+    if (!sendSms()) {
+      retrying = true;
+    }
   }
 }
 
 void loop() {
+  if (retrying) {
+    retrySend();
+  } else {
+    sleepUntilButtonPressOrTimeout();
+  }
+}
+
+void sleepUntilButtonPressOrTimeout() {
   // Sleep until the button is pressed. Using SLEEP_NETWORK_STANDBY for now to
   // avoid our SIM being blocked by the network while testing, see docs:
   // https://docs.particle.io/reference/device-os/firmware/boron/#sleep-sleep-
@@ -40,15 +54,39 @@ void loop() {
       /* sleep mode = */ SLEEP_NETWORK_STANDBY,
       /* seconds (0 = no timeout) = */ 0);
 
-  if (sleep_result.wokenUpByPin()) {
+  if (!sleep_result.wokenUpByPin()) {
     // Only send SMS if we're woken up by the button being pressed. We don't set
     // a timeout, but docs say the Boron won't sleep for more than 24 days in a
     // row.
-    sendSms();
+    return;
+  }
+
+  Serial.println("MailBoy detected button press, attempting send...");
+  if (!sendSms()) {
+    retrying = true;
   }
 }
 
-void sendSms() {
-  Serial.println("Sending SMS.");
-  Particle.publish("twilio_sms", "You have mail!", PRIVATE);
+void retrySend() {
+  Serial.println("MailBoy sleeping for a minute and then retrying send...");
+  // TODO: figure out a better combination of arguments to ensure we don't wake
+  // up for pin interactions.
+  System.sleep(
+      /* wakeUpPin = */ BUTTON_PIN, /* edgeTriggerMode = */ FALLING,
+      /* sleep mode = */ SLEEP_NETWORK_STANDBY,
+      /* seconds (0 = no timeout) = */ 60);
+
+  Serial.println("MailBoy retrying send...");
+  if(sendSms()) {
+    retrying = false;
+  }
+}
+
+bool sendSms() {
+  Serial.println("Attempting to send SMS.");
+  bool success = Particle.publish("twilio_sms", "You have mail!", PRIVATE);
+  if (!success) {
+    Serial.println("Failed!");
+  }
+  return success;
 }
